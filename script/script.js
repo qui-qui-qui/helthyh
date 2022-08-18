b628b2c2540b4688afae316d34bb78143d0f672f
Даниил Родионов <zenitpv@icloud.com>
2022-08-18 21:21:14 +0300

    18.08 add 2 photos and add action to profile-btn

--- script/script.js
+++ script/script.js
@@ -33,4 +33,28 @@
       ticking = true;
     });
   })();
+
+  // change profile img
   
+window.addEventListener('load', function() {
+    document.querySelector('.add-button input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            let img = document.querySelector('.profile-picture img');
+            let imgArr = document.querySelectorAll('.profile-picture img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+            }
+            
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          imgArr[1].src = img.src; 
+        }
+        
+    });
+  });
+
+
+// calendar
+// change data
+const theDay = document.querySelector('.the-day');
+
+theDay.innerHTML = new Date().toLocaleDateString().slice(0,2);
