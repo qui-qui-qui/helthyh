e8d7187b763281f328d8aa595baa66f07eb884bd
TurnToDie <turn_to_die@mail.ru>
2022-08-30 18:05:50 +0300

    add BaseWidget model

--- healtyh_django/widgets/models.py
+++ healtyh_django/widgets/models.py
@@ -1,1 +1,11 @@
+from turtle import position, width
 from django.db import models
+
+class BaseWidget(models.Model):
+    position = models.IntegerField(verbose_name='Положение', unique=True, blank=False, null=False)
+    heigth = models.FloatField(verbose_name='Высота', blank=False)
+    width = models.FloatField(verbose_name='Ширина', blank=False)
+    mode = models.BooleanField(verbose_name='Присутствие', blank=False)
+
+    class Meta:
+        db_table = 'base_widget'
